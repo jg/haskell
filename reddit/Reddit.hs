354191f22207ca187489effd01a5d2ee54531a36
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-07-19 12:22:31 +0200

    Add persisting comments

--- reddit/Reddit.hs
+++ reddit/Reddit.hs
@@ -20,19 +20,41 @@ import Data.Tree.NTree.TypeDefs
 import Text.JSON.Generic
 import Safe
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.ToRow
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.FromRow
 import Data.Hash.MD5
+import Data.Maybe
+import Control.Applicative
+import Data.Time.Clock
 
 newtype Url = Url String deriving (Show)
 
 newtype User = User Text
 
-newtype Hash = Hash String deriving (Show)
+newtype Hash = Hash String
+
+instance Show Hash where
+  show (Hash s) = s
 
 data Comment = Comment {
+  id :: Maybe Integer,
   title :: String,
   subreddit :: String,
-  body :: String
+  body :: String,
+  hash :: Maybe String,
+  created_at :: Maybe UTCTime,
+  updated_at :: Maybe UTCTime
   } deriving (Show, Typeable, Data)
+
+-- Simple Comment constructor, omits all of the db fluff
+newComment :: String -> String -> String -> Comment
+newComment title subreddit body =
+  Comment Nothing title subreddit body Nothing Nothing Nothing
+
+instance FromRow Comment where
+  fromRow = Comment <$>
+            field <*> field <*> field <*> field <*> field <*> field <*> field
 
 userToUrl :: String -> Url
 userToUrl u =
@@ -71,24 +93,54 @@ getComments url maxPages comments = do
     Just href -> do
       print href
       getComments (Url href) (maxPages-1) (comments ++ pageComments)
-    Nothing -> return (comments)
+    Nothing -> do
+      print "nothing!"
+      return (comments)
 
 getCommentPages :: Int -> IO [Comment]
 getCommentPages pages = getComments (Url "http://www.reddit.com/user/dons") pages []
 
 commentHash :: Comment -> Hash
-commentHash (Comment title subreddit body) =
+commentHash (Comment _ title subreddit body _ _ _) =
   Hash $ md5s (Str (title ++ subreddit ++ body))
+
+foo :: IO ()
 foo = do
-  comments <- getCommentPages 100
-  writeFile "comments.json" (encodeJSON comments)
+  conn <- connectPostgreSQL connectionUrl
+  comments <- getCommentPages 1
+  mapM (persistComment conn) comments
+  close conn
+  return ()
 
 connectionUrl :: B.ByteString
-connectionUrl = "host=localhost port=5432 dbname=reddit"
-persistComment :: Comment -> IO ()
-persistComment c@(Comment title subreddit body) = do
+connectionUrl = "host=localhost port=5432 dbname=reddit password=testtest"
+
+getRandomComment :: IO (Maybe Comment)
+getRandomComment = do
   conn <- connectPostgreSQL connectionUrl
+  r <- query_ conn queryText :: IO [Comment]
+  close conn
+  return (headMay r)
+  where
+    queryText = "select * from comments order by random() limit 1"
+
+bar = do
+  conn <- connectPostgreSQL connectionUrl
+  commentMay <- getRandomComment
+  close conn
+  isCommentInDb conn (fromJust commentMay)
+
+isCommentInDb :: Connection -> Comment -> IO Bool
+isCommentInDb conn c = do
+  r <- query conn "select id from comments where hash = ?" (hash c) :: IO [(Only Int)]
+  print (fromOnly (hash c))
+  return $ (isJust . headMay) r
+  where
+    hash :: Comment -> Only String
+    hash = Only . show . commentHash
+
+persistComment :: Connection -> Comment -> IO ()
+persistComment conn c@(Comment _ title subreddit body _ _ _) = do
   execute conn "INSERT INTO comments(title,subreddit,body,hash) VALUES (?, ?, ?, ?)"
     (title, subreddit, body, show (commentHash c))
-  close conn
   return ()
