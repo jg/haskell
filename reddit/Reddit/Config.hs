039fd56781c8fd1efd6c03718ef2a306c794aad0
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-07-20 18:45:10 +0200

    Get some encapsulation going

--- reddit/Reddit/Config.hs
+++ reddit/Reddit/Config.hs
@@ -1,69 +1,24 @@
-module Reddit.Config where
+module Reddit.Config
+       (
+         Config,
+         connectionUrl,
+         readConfig
+       ) where
 
-import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Prim
-import Text.ParserCombinators.Parsec.Char
-import Data.Char
-import Data.Maybe
-import Text.Parsec.Error
 import qualified Data.Map as Map
 import Control.Monad.Reader
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
 
-type Config = Map.Map String String
+import Reddit.Config.Parser
 
-ident :: Parser String
+newtype Config = Config (Map.Map String String)
 
-ident = do
-  c <- letter
-  cs <- many (letter <|> digit <|> char '_')
-  return (c:cs)
-  <?> "identifier"
-
-comment :: Parser ()
-
-comment = do
-  char '#'
-  skipMany (noneOf ("\r\n"))
-  <?> "comment"
-
-eol :: Parser ()
-
-eol = do
-  oneOf "\n\r"
-  return ()
-  <?> "end of line"
-
-item :: Parser (String, String)
-item = do
-  key <- ident
-  skipMany space
-  char ':'
-  skipMany space
-  value <- manyTill anyChar (try eol <|> try comment <|> eof)
-  return (key, rstrip value)
-  where rstrip = reverse . dropWhile isSpace . reverse
-            
-
-line :: Parser (Maybe (String, String))
-line = do
-  skipMany space
-  try (comment >> return Nothing) <|> (item >>= return . Just)
-
-
-file :: Parser [(String, String)]
-file = do
-  lines <- many line
-  return (catMaybes lines) 
-
-readConfigFile :: SourceName -> IO (Either ParseError Config)
-readConfigFile name =
-  parseFromFile file name >>=
-  return . fmap (foldr (uncurry Map.insert) Map.empty . reverse) 
-
-lookupVar :: Config -> String -> String
-lookupVar config name = fromJust (Map.lookup name config)
+readConfig :: IO (Config)
+readConfig = do
+  result <- readConfigFile "config.cfg"
+  let Right map = result in return (Config map)
 
 connectionUrl :: ReaderT Config IO B.ByteString
 connectionUrl = do
@@ -75,4 +30,5 @@ connectionUrl = do
     url config = unwords $
       zipWith (\a b -> a ++ "=" ++ b) names (values config)
 
-readConfig = readConfigFile "config.cfg"
+lookupVar :: Config -> String -> String
+lookupVar config name = fromJust (Map.lookup name config)
---
+++ reddit/Reddit/Config/Parser.hs
@@ -0,0 +1,60 @@
+module Reddit.Config.Parser
+       (
+         readConfigFile
+       ) where
+
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Prim
+import Text.ParserCombinators.Parsec.Char
+import Data.Char
+import Data.Maybe
+import Text.Parsec.Error
+import qualified Data.Map as Map
+
+ident :: Parser String
+
+ident = do
+  c <- letter
+  cs <- many (letter <|> digit <|> char '_')
+  return (c:cs)
+  <?> "identifier"
+
+comment :: Parser ()
+
+comment = do
+  char '#'
+  skipMany (noneOf ("\r\n"))
+  <?> "comment"
+
+eol :: Parser ()
+
+eol = do
+  oneOf "\n\r"
+  return ()
+  <?> "end of line"
+
+item :: Parser (String, String)
+item = do
+  key <- ident
+  skipMany space
+  char ':'
+  skipMany space
+  value <- manyTill anyChar (try eol <|> try comment <|> eof)
+  return (key, rstrip value)
+  where rstrip = reverse . dropWhile isSpace . reverse
+            
+
+line :: Parser (Maybe (String, String))
+line = do
+  skipMany space
+  try (comment >> return Nothing) <|> (item >>= return . Just)
+
+file :: Parser [(String, String)]
+file = do
+  lines <- many line
+  return (catMaybes lines) 
+
+readConfigFile :: SourceName -> IO (Either ParseError (Map.Map String String))
+readConfigFile name =
+  parseFromFile file name >>=
+  return . fmap (foldr (uncurry Map.insert) Map.empty . reverse) 
